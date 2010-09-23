88fb4fa93a34b84ce34b8cabce5e5cf6ad548183
"Charles Killian ext:(%22) <ckillian@cs.purdue.edu>
2010-09-23 09:25:12 -0400

    Add flag for whether to downgrade to NONE or READ.  Turn off fflush when logging is enabled.

--- application/appgroupstream/generate.py
+++ application/appgroupstream/generate.py
@@ -18,8 +18,13 @@ bootgroup = ""
 #gdb_cmd = "gdb --command=/homes/yoo7/scratch/mace-project/mace/application/appgroupstream/gdb_cmd.txt --args "
 gdb_cmd = ""
 logging_para = ""
+downgrade_to_none = ""
 if int(sys.argv[5]) == 1:
-  logging_para = " -MACE_LOG_LEVEL 1 -MACE_LOG_AUTO_ALL 1 "
+  #logging_para = " -MACE_LOG_LEVEL 1 -MACE_LOG_AUTO_SELECTORS GenericTreeMulticast -MACE_LOG_ASYNC_FLUSH 1 "
+  logging_para = " -MACE_LOG_LEVEL 1 -MACE_LOG_AUTO_ALL 1 -MACE_LOG_ASYNC_FLUSH 1 "
+
+if int(sys.argv[6]) == 1:
+  downgrade_to_none = " -DOWNGRADE_TO_NONE 1 "
 
 for i in range(0,node):
 	if allgroup == "":
@@ -35,7 +40,7 @@ for i in range(0,node):
 
 	if i == node-1:
 		#print 'cd '+macedir+'/build/mace/application/appgroupstream; ./groupstream -ALL_NODES "' + allgroup + '"  -MACE_ADDRESS_ALLOW_LOOPBACK 1 -MACE_PORT '+str(ip_start + i*(num_transport*ip_interval)) + " -NUM_MESSAGES "+str(num_messages)+" -NUM_NODES "+str(node)+" -NUM_TRANSPORT "+str(num_transport)+" -IP_INTERVAL "+str(ip_interval)+" -DELAY $1 -PERIOD $2 -CURRENT "+str(current)+" 2>&1 | tee log_"+str(node)+"_"+str(num_transport)+"_$1_$2_" + str(i) + ".txt \n"
-		print 'mkdir '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; cd '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; '+gdb_cmd+'../groupstream -BOOTSTRAP_NODES "' + bootgroup + '" -ALL_NODES "' +allgroup + '" ' + logging_para + ' -MACE_ADDRESS_ALLOW_LOOPBACK 1 -MACE_PORT '+str(ip_start + i*(num_transport*ip_interval)) + " -NUM_MESSAGES "+str(num_messages)+" -NUM_NODES "+str(node)+" -NUM_TRANSPORT "+str(num_transport)+" -IP_INTERVAL "+str(ip_interval)+" -DELAY $1 -PERIOD $2 -CURRENT "+str(current)+" 2>&1 | tee ../log_"+str(node)+"_"+str(num_transport)+"_$1_$2_" + str(i) + ".txt | egrep -v \"TRACE|ServiceStack|demux|routine|ThreadPool|DeliveryTransport|BaseTransport|TcpTransport|UdpTransport|SNAPSHOT|TcpConnection|expire_printer|AgentLock|Ticket\" \n"
+		print 'mkdir -p '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; cd '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; rm ./*; '+gdb_cmd+'../groupstream -BOOTSTRAP_NODES "' + bootgroup + '" -ALL_NODES "' +allgroup + '" ' + logging_para + downgrade_to_none + ' -MACE_ADDRESS_ALLOW_LOOPBACK 1 -MACE_PORT '+str(ip_start + i*(num_transport*ip_interval)) + " -NUM_MESSAGES "+str(num_messages)+" -NUM_NODES "+str(node)+" -NUM_TRANSPORT "+str(num_transport)+" -IP_INTERVAL "+str(ip_interval)+" -DELAY $1 -PERIOD $2 -CURRENT "+str(current)+" 2>&1 | tee ../log_"+str(node)+"_"+str(num_transport)+"_$1_$2_" + str(i) + ".txt | egrep -v \"TRACE|DEBUG|BaseMaceService|ServiceStack|demux|routine|ThreadPool|DeliveryTransport|BaseTransport|TcpTransport|UdpTransport|SNAPSHOT|TcpConnection|expire_printer|AgentLock|Ticket\" \n"
 	else:
-		print 'mkdir '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; cd '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; '+gdb_cmd+'../groupstream -BOOTSTRAP_NODES "' + bootgroup + '" -ALL_NODES "' + allgroup + '" ' + logging_para + ' -MACE_ADDRESS_ALLOW_LOOPBACK 1 -MACE_PORT '+str(ip_start + i*(num_transport*ip_interval)) + " -NUM_MESSAGES "+str(num_messages)+" -NUM_NODES "+str(node)+" -NUM_TRANSPORT "+str(num_transport)+" -IP_INTERVAL "+str(ip_interval)+" -DELAY $1 -PERIOD $2 -CURRENT "+str(current)+" > ../log_"+str(node)+"_"+str(num_transport)+"_$1_$2_" + str(i) + ".txt 2>&1 &\n"
+		print 'mkdir -p '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; cd '+macedir+'/build/mace/application/appgroupstream/'+str(i)+'; rm ./*; '+gdb_cmd+'../groupstream -BOOTSTRAP_NODES "' + bootgroup + '" -ALL_NODES "' + allgroup + '" ' + logging_para + downgrade_to_none + ' -MACE_ADDRESS_ALLOW_LOOPBACK 1 -MACE_PORT '+str(ip_start + i*(num_transport*ip_interval)) + " -NUM_MESSAGES "+str(num_messages)+" -NUM_NODES "+str(node)+" -NUM_TRANSPORT "+str(num_transport)+" -IP_INTERVAL "+str(ip_interval)+" -DELAY $1 -PERIOD $2 -CURRENT "+str(current)+" > ../log_"+str(node)+"_"+str(num_transport)+"_$1_$2_" + str(i) + ".txt 2>&1 &\n"
 
--- application/appgroupstream/generate.sh
+++ application/appgroupstream/generate.sh
@@ -1,6 +1,6 @@
 if [ $# -gt 3 ]
 then
-python generate.py $1 $2 $5 `date "+%s.%N"` $6 > ../../../build/mace/application/appgroupstream/run_$1_$2.sh
+python generate.py $1 $2 $5 `date "+%s.%N"` $6 $7 > ../../../build/mace/application/appgroupstream/run_$1_$2.sh
 chmod 777 ../../../build/mace/application/appgroupstream/run_$1_$2.sh
 cd ../../../build/mace/application/appgroupstream
 ./run_$1_$2.sh $3 $4
