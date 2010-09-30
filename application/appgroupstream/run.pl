d53363f8d95cbe78f751fbd5ba7fca5722b216a3
"Charles Killian ext:(%22) <ckillian@cs.purdue.edu>
2010-09-30 11:52:05 -0400

    add scripts for running groupstream2 on the cluster

---
+++ application/appgroupstream/run.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w 
+
+use strict;
+
+my $nodes_per_host = 4;
+my $num_threads = 16;
+my $sleep_time = 100000;
+my $interval = 24000000;
+my $num_messages = 10;
+my $logging = 0;
+my $downgrade_to_none = 0;
+
+my $current_time = (time()+10);
+
+system(qq{python generatep.py $nodes_per_host $num_threads $num_messages $current_time $logging $downgrade_to_none > /homes/ckillian/groupstream/run_${nodes_per_host}_${num_threads}.sh});
+system(qq{chmod 777 /homes/ckillian/groupstream/run_${nodes_per_host}_${num_threads}.sh});
+# cd /homes/ckillian/groupstream/
+exec("python /homes/ckillian/src/pssh-1.4.3/bin/pssh -v -P -t 700 -h /homes/ckillian/groupstream/hosts.pssh /homes/ckillian/groupstream/run_${nodes_per_host}_${num_threads}.sh $sleep_time $interval");
+#pkill groupstream
