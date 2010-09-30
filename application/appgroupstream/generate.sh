5b94258f71d624e6f005ff4e8e8a2c4276ee67c7
"Charles Killian ext:(%22) <ckillian@cs.purdue.edu>
2010-09-30 11:51:21 -0400

    kill groupstream2

--- application/appgroupstream/generate.sh
+++ application/appgroupstream/generate.sh
@@ -5,6 +5,7 @@ chmod 777 ../../../build/mace/application/appgroupstream/run_$1_$2.sh
 cd ../../../build/mace/application/appgroupstream
 ./run_$1_$2.sh $3 $4
 pkill groupstream
+pkill groupstream2
 else
 echo "Usage : ./test.sh [num_nodes] [num_transport] [delay] [period] [num_messages] [logging (0|1)]"
 fi
