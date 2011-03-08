d6a332583048ca749a108a1346f63b54e7435be8
"Charles Killian ext:(%22) <ckillian@cs.purdue.edu>
2011-03-08 13:18:58 -0500

    Small tweaks in the running of appgroupstream.

--- application/appgroupstream/run.pl
+++ application/appgroupstream/run.pl
@@ -3,7 +3,7 @@
 use strict;
 
 my $nodes_per_host = 4;
-my $num_threads = 16;
+my $num_threads = $ARGV[0] || 32;
 my $sleep_time = 100000;
 my $interval = 24000000;
 my $num_messages = 10;
@@ -11,6 +11,8 @@ my $logging = 0;
 my $downgrade_to_none = 0;
 
 my $current_time = (time()+10);
+print "Running nodes_per $nodes_per_host num_threads $num_threads current_time $current_time downgrade_to_none $downgrade_to_none";
+system(qq{echo "Running nodes_per $nodes_per_host num_threads $num_threads current_time $current_time downgrade_to_none $downgrade_to_none" >> runs.txt});
 
 system(qq{python generatep.py $nodes_per_host $num_threads $num_messages $current_time $logging $downgrade_to_none > /homes/ckillian/groupstream/run_${nodes_per_host}_${num_threads}.sh});
 system(qq{chmod 777 /homes/ckillian/groupstream/run_${nodes_per_host}_${num_threads}.sh});
