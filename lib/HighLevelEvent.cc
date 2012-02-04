d310d146f4b86989fe47bd44667d699d5ba0e696
chuangw <none@none>
2012-02-04 18:27:43 -0500

    Highlevel Migration event
    I want to make it simpler such that at any time there is only one migration event takes place.
    
    It perhaps would better to have separate migration events: one for migrating only particular contexts, one for migrating the entire node.

--- lib/HighLevelEvent.cc
+++ lib/HighLevelEvent.cc
@@ -3,5 +3,4 @@
 pthread_mutex_t mace::HighLevelEvent::eventMutex = PTHREAD_MUTEX_INITIALIZER;
 uint64_t mace::HighLevelEvent::nextTicketNumber = 1;
 uint64_t mace::HighLevelEvent::now_committing = 1;
-uint64_t mace::HighLevelEvent::migrationEventID = 0;
-pthread_cond_t mace::HighLevelEvent::migrationCond = PTHREAD_COND_INITIALIZER;
+std::queue<pthread_cond_t*> mace::HighLevelEvent::migrationRequests;
