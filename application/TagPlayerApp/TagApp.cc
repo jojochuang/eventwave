967b06446bd7c21e56034a3eb9758548483a1e67
liu473 <none@none>
2012-04-16 17:44:03 -0400

    changed services/TagPlayer/TagPlayer.mac changed services/TagPlayer/TagPlayerServiceClass.mh changed application/TagPlayerApp/TagApp.cc

--- application/TagPlayerApp/TagApp.cc
+++ application/TagPlayerApp/TagApp.cc
@@ -1,57 +1,71 @@
 #include "SysUtil.h"
 #include "Util.h"
- 
+#include "TagPlayerServiceClass.h"
+#include "lib/mace.h"
+#include "lib/params.h"
+#include "TagPlayer-init.h"
+#include "load_protocols.h"
+
 using namespace std;
+
+#define BUILDING_NUM 3
+#define ROOM_NUM 2
  
 class TagPlayerResponseHandler :public TagPlayerDataHandler {
    void printmsg(mace::string message,registration_uid_t rid){
-     maceout << " MESSAGE: " << message <<endl;
+     cout << " MESSAGE: " << message <<endl;
    }
    void printkidNum(uint16_t kidNum, registration_uid_t rid) {
-     maceout << " How many kids in this room: " << kidNum <<endl;
+     cout << " How many kids in this room: " << kidNum <<endl;
    }
-}
+};
  
 int main(int argc, char* argv[])
 {
-  time_t time0, time1;
+  
+  //time_t time0, time1;
   uint16_t direction;
   uint16_t newBuilding, newRoom;
+  uint16_t node_id,action;
   
-  //store all the players' ID and the MaceKey, use parameter.default
+  params::loadparams(argc,argv);
 
+  load_protocols();
   //initial the service
   TagPlayerResponseHandler tprh;
   TagPlayerServiceClass& tagplayer = TagPlayer_namespace::new_TagPlayer_TagPlayer();
   tagplayer.maceInit();
   tagplayer.registerUniqueHandler(tprh);
-
-  //start the testing timer
-  time0 = time(NULL);
+  
   while(1){
-   time1 = time(NULL);
-   //pick up a player randomly to chat
-   tagplayer.sendmsg(/*MaceKey of the player that want to chat*/);
-
-   if(difftime(time1,time0)>=5)
-   {
-     //random move
      srand(time(NULL));
-     direction = rand()%5;
-     tagPlayer.movePlayer(direction);
+     node_id = rand()%10;
+     srand(time(NULL));
+     action = rand()%3;
+     if(action==0)
+     {
+     	//tagplayer.movePlayer(node)id+1,direction);
+        srand(time(NULL));
+        direction = rand()%5;
+     	tagplayer.movePlayer(node_id+1,direction);
+        
+     }
+     else if(action==1)
+     {
+       srand(time(NULL));
+       newBuilding = rand()% BUILDING_NUM;
+       newRoom = rand()%ROOM_NUM;
+       tagplayer.changeRoom(node_id+1,newBuilding,newRoom);
+       
+     }
+     else if(action==2)
+     {
+       srand(time(NULL));
+       newBuilding = rand()% BUILDING_NUM;
+       newRoom = rand()%ROOM_NUM;
+       tagplayer.checkKidNum(newBuilding,newRoom);
 
-     
-   }
-   if(difftime(time1,time0)>=20)
-   {
-      srand(time(NULL));
-      newBuilding = rand()%/*building number*/;
-      newRoom = rand()%/*Room number*/;
-     //random change room
-     tagPlayer.changeRoom(newBuilding,newRoom);
-     tagPlayer.checkKidNum(newBuilding,newRoom);
-   }
-   time0 = time1;
+     }
   }
   
   SysUtil::sleep();						                          
