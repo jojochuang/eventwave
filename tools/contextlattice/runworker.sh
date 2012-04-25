6f218948240b00fe056c0a2823ee67ed4aa41820
chuangw <none@none>
2012-04-25 13:37:24 -0400

    Update execution script.

--- tools/contextlattice/runworker.sh
+++ tools/contextlattice/runworker.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-#for i in {1..$2}
-BASE=0
-for (( i=1+BASE; i<= $1+BASE ; i++))
+# TODO: evenly distribute nodes onto ten machines
+BASE=500
+HOSTS=10
+DIV=`expr  $1 / $HOSTS `
+MOD=`expr $1 % $HOSTS `
+#for (( i=1+BASE; i<= $1+BASE ; i+=DIV))
+procIDStart=1
+procIDEnd=0
+for (( hostNo=1; hostNo<= $HOSTS; hostNo++ ))
 do
-    #echo $i
-    ./heartbeat -isworker 1 -pid $i -TRACE_ALL 0 -TRACE_SUBST "SpawnProcess UpdateContextMapping  ReportJobMigration NotifySignal" &> /tmp/unit_app$i.log &
+    machineID=`printf "%02d" $hostNo`
+
+    if [ $1 -le $HOSTS ]
+    then
+        procIDStart=$hostNo
+        procIDEnd=$hostNo
+    else
+        procIDStart=`expr $procIDEnd + 1`
+        if [ $hostNo -le $MOD ]
+        then
+            procIDEnd=`expr $procIDEnd + $DIV + 1`
+        else
+            procIDEnd=`expr $procIDEnd + $DIV`
+        fi
+    fi
+
+    if [ $hostNo -le $1 ]
+    then
+        echo "ssh cloud"$machineID".cs.purdue.edu ./multiworker.sh $procIDStart $procIDEnd &" | /bin/bash
+    fi
 done
