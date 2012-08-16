398ec2b9cc2f7d053edf938d0a6fddbf5d11eba5
chuangw <none@none>
2012-08-16 17:05:08 -0400

    HighLevelEvent is PrintPrintable now. Make it easier to debug/log.

--- lib/HighLevelEvent.cc
+++ lib/HighLevelEvent.cc
@@ -15,3 +15,41 @@ const uint8_t mace::HighLevelEvent::DOWNCALLEVENT= 5;
 const uint8_t mace::HighLevelEvent::MIGRATIONEVENT = 6;
 const uint8_t mace::HighLevelEvent::NEWCONTEXTEVENT = 7;
 const uint8_t mace::HighLevelEvent::UNDEFEVENT = 8;*/
+
+void mace::HighLevelEvent::print(std::ostream& out) const {
+  out<< "HighLevelEvent(";
+  out<< "eventID="; mace::printItem(out, &(eventID) ); out<<", ";
+  out<< "eventType="; 
+  
+  switch( eventType ){
+    case STARTEVENT: out<<"STARTEVENT"; break;
+    case ENDEVENT: out<<"ENDEVENT"; break;
+    case TIMEREVENT: out<<"TIMEREVENT"; break;
+    case ASYNCEVENT: out<<"ASYNCEVENT"; break;
+    case UPCALLEVENT: out<<"UPCALLEVENT"; break;
+    case DOWNCALLEVENT: out<<"DOWNCALLEVENT"; break;
+    case MIGRATIONEVENT: out<<"MIGRATIONEVENT"; break;
+    case NEWCONTEXTEVENT: out<<"NEWCONTEXTEVENT"; break;
+    case UNDEFEVENT: out<<"UNDEFEVENT"; break;
+    default: mace::printItem(out, &(eventType) ); break;
+  }
+  
+  out<<", ";
+
+  out<< "eventContexts="; mace::printItem(out, &(eventContexts) ); out<<", ";
+  out<< "eventMessageCount="; mace::printItem(out, &(eventMessageCount) ); out<<", ";
+  out<< "eventContextMappingVersion="; mace::printItem(out, &(eventContextMappingVersion) );
+  out<< ")";
+
+} // print
+
+void mace::HighLevelEvent::printNode(PrintNode& pr, const std::string& name) const {
+  mace::PrintNode printer(name, "HighLevelEvent" );
+  
+  mace::printItem( printer, "eventID", &eventID );
+  mace::printItem( printer, "eventType", &eventType );
+  mace::printItem( printer, "eventContexts", &eventContexts );
+  mace::printItem( printer, "eventMessageCount", &eventMessageCount );
+  mace::printItem( printer, "eventContextMappingVersion", &eventContextMappingVersion );
+  pr.addChild( printer );
+}
