1a3f4a89597785aec867424b9abb9dae615f3feb
Wei-Chiu Chuang <weichiu@purdue.edu>
2013-02-12 23:59:44 -0500

    Added a new perl script for automatically testing ContextLattice with different versions of gcc.
    The perl script is better than the shell script. Don't use the shell one, it will be deprecated.
    
    --HG--
    branch : chuangw/unit_test

--- tools/gcc_test/compilemace.sh
+++ tools/gcc_test/compilemace.sh
@@ -4,11 +4,12 @@
 gcc_dir=/scratch/chuangw/opt
 parallel_build=4
 
-for ver in $gcc_dir/*
+#for ver in $gcc_dir/*
+for ver in $(find $gcc_dir/* -maxdepth 0 -type d);
 do
   echo "Compile ContextLattice using " $ver
-  make clean
-  cmake  -D CMAKE_CXX_COMPILER=$ver/bin/g++ ..
-  make -j $parallel_build
-  make test
+  make clean 2>&1 | tee $ver.log
+  cmake  -D CMAKE_CXX_COMPILER=$ver/bin/g++ .. 2>&1 | tee -a $ver.log
+  make -j $parallel_build 2>&1 | tee -a $ver.log
+  make test 2>&1 | tee -a $ver.log
 done
